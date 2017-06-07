1c9cb0f95744b1d5fc8aca67a522e35fcf92b60d
Umar Arfeen <Anon-Exploiter@users.noreply.github.com>
2017-06-07 06:00:16 -0700

    Create errors.php

---
+++ internels/errors.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+_______________.___.
+\______   \__  |   |
+ |    |  _//   |   |
+ |    |   \\____   |
+ |______  // ______|
+        \/ \/       
+   _____         _______           ________        __________.__         ._____________   __________ 
+  /  _  \   ____ \   _  \   ____   \_____  \___  __\______   |  |   ____ |__\__    _______\______   \
+ /  /_\  \ /    \/  /_\  \ /    \    _(__  <\  \/  /|     ___|  |  /  _ \|  | |    |_/ __ \|       _/
+/    |    |   |  \  \_/   |   |  \  /       \>    < |    |   |  |_(  <_> |  | |    |\  ___/|    |   \
+\____|__  |___|  /\_____  |___|  / /______  /__/\_ \|____|   |____/\____/|__| |____| \___  |____|_  /
+        \/     \/       \/     \/         \/      \/                                     \/       \/ 
+
+                                ~ Changing Author Name Wont Make You One :) 
+                                             #Respect Coders
+*/
+
+$errno = "\t{=} Please Enter A Website :/\n\t\t~ An0n 3xPloiTeR\n";
+
+$val_error = "\t{+} Please Use Numeric Value :)\n\t\t~ An0n 3xPloiTeR\n";
+
+$val_select = "\t{\$} Please Use The Index Value From The List\n\t   [+] Not By Your Own :/\n\t\t ~ An0n 3xPloiTeR  \n";
+
+?>
