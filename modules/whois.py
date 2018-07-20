c05fe81dede38d64af89d08ba43994d5701efe5b
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2018-07-20 15:44:22 +0500

    Update whois.py
    Fixed, API key issue. Using an alternate website!

--- modules/whois.py
+++ modules/whois.py
@@ -23,10 +23,23 @@ import re, 		 os
 import requests, json
 
 def whoIS(website):
-	website = "http://api.whoapi.com/?apikey=66ca3039356c0287ff63ed472f528478&r=whois&domain={url}&ip=".format(url=website)
-	req = Request(website, _timeout=8, _encode=True)
-	js = json.loads(req)
-	whois = js['whois_raw']
-	for result in whois.split("\n"):
-		if len(result) != 0:
-			write(var="~", color=c, data=result)
+	website 	= removeHTTP(website)
+	url 		= "https://www.whois.com/whois/{site}".format(site=website)
+	try:
+		request 	= Request(url, _timeout=5, _encode=None)
+		bs 		= BeautifulSoup(request, 'html.parser')
+		result 		= bs.find_all('pre', {'class': 'df-araw'})[0].text.encode('UTF-8')
+		print("\r{output}".format(output=c + result))
+	except:
+		write(var="!", color=r, data="Sorry, whois cannot be performed right now! :[")
+
+
+## Lazy to add a new API key. So, added an good alternate method above! xD
+## Old Code can be found below!
+# website = "http://api.whoapi.com/?apikey=66ca3039356c0287ff63ed472f528478&r=whois&domain={url}&ip=".format(url=website)
+# req = Request(website, _timeout=8, _encode=True)
+# js = json.loads(req)
+# whois = js['whois_raw']
+# for result in whois.split("\n"):
+# 	if len(result) != 0:
+# 		write(var="~", color=c, data=result)
